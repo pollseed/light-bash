73a6de07fe9faee270a3dc779df116b32b94857e
pollseed <cb.so.devope+0001@gmail.com>
2015-04-26 00:48:52 +0900

    add g common divisor

--- lib/mk_gcommondivisor.bash
+++ lib/mk_gcommondivisor.bash
@@ -3,11 +3,54 @@
 if [ -z "$1" ] ; then
   echo 0
 else
+  # 第１引数の公約数
+  FIRST=$1
   first_array=()
-  for i in {0.."$1"} ; do
-    if ("$1" % ${i} == 0) ; then
-      ${first_array}[${#first_array[@]}] = ${i}
+  i=1
+  while [ ${i} -le ${FIRST} ] ; do
+    if [ "$(( ${FIRST} % ${i} ))" -eq 0 ] ; then
+      first_array+=(${i})
     fi
+    let i++
   done
-  echo first_array
+
+  # 第２引数の公約数
+  SECOND=$2
+  second_array=()
+  i=1
+  while [ ${i} -le ${SECOND} ] ; do
+    if [ "$(( ${SECOND} % ${i} ))" -eq 0 ] ; then
+      second_array+=(${i})
+    fi
+    let i++
+  done
+
+  # 最大公約数を出力
+  g_common_divisor=0
+  i=0
+  for j in ${first_array[@]}; do
+    for k in ${second_array[@]}; do
+      if [ ${j} -eq ${k} ] ; then
+        g_common_divisor=${j}
+      fi
+      let k++
+    done
+    let i++
+  done
+  echo ${g_common_divisor}
+  
+  # debug
+  #echo DEBUG_LOG -----
+  #i=0
+  #for j in ${first_array[@]}; do
+  #  echo ${j}
+  #  let i++
+  #done
+  
+  #i=0
+  #for j in ${second_array[@]}; do
+  #  echo ${j}
+  #  let i++
+  #done
 fi
+
