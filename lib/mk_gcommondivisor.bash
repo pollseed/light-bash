6d52ee05bfdc5eaa98c39516820d0e08fc0b2f7a
pollseed <cb.so.devope+0001@gmail.com>
2015-04-27 02:03:19 +0900

    bug fix

--- lib/mk_gcommondivisor.bash
+++ lib/mk_gcommondivisor.bash
@@ -1,6 +1,10 @@
 #!/bin/bash
+#
+# args $1 $2 $1と$2の最大公約数を出力
+# 引数が2つ指定されなかった場合は0が出力されます.
+#
 
-if [ -z "$1" ] ; then
+if [ -z "$1" ] || [ $# -ne 2 ] ; then
   echo 0
 else
   # 第１引数の公約数
@@ -27,15 +31,13 @@ else
 
   # 最大公約数を出力
   g_common_divisor=0
-  i=0
-  for j in ${first_array[@]}; do
-    for k in ${second_array[@]}; do
-      if [ ${j} -eq ${k} ] ; then
-        g_common_divisor=${j}
+  for i in ${first_array[@]}; do
+    for j in ${second_array[@]}; do
+      if [ ${i} -eq ${j} ] ; then
+        g_common_divisor=${i}
       fi
-      let k++
+      let j++
     done
-    let i++
   done
   echo ${g_common_divisor}
   
